a51a64ac552cd7fba69bafd5058e804fcc6f4ac7
anibal muñoz <2113110302@untels.edu.pe>
2023-11-20 11:19:35 -0500

    UC3-Parte02-Muñoz

--- miapp/views.py
+++ miapp/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return render(request,"index.html")
+def cursos(request):
+    cursos = ['Lenguaje de Programacion 3', 'Legislacion Informatica', 
+              'Ingenieria de Requerimientos', 'Algoritmos de Computacion Grafica', 
+              'Microprocesadores', 'Gestion de Procesos de Negocios', 'Dinamica de Sistemas']
+    return render(request, 'cursos.html', {'cursos': cursos})
